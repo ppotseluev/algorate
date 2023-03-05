26bf6b5ca6f82e96f5394c62fff2f57359dce1c3
ppotseluev <potseluev-pasha@yandex.ru>
2023-03-05 11:06:04 +0800

    handle errors in event sink

--- trader-lib/src/main/scala/com/github/ppotseluev/algorate/trader/akkabot/EventsSink.scala
+++ trader-lib/src/main/scala/com/github/ppotseluev/algorate/trader/akkabot/EventsSink.scala
@@ -1,28 +1,37 @@
 package com.github.ppotseluev.algorate.trader.akkabot
 
+import cats.ApplicativeThrow
+import cats.effect.kernel.Sync
+import cats.implicits._
 import com.github.ppotseluev.algorate.charts.TradingCharts
 import com.github.ppotseluev.algorate.math.PrettyDuration._
+import com.github.ppotseluev.algorate.trader.LoggingSupport
 import com.github.ppotseluev.algorate.trader.akkabot.Event.TradingSnapshot
-import com.github.ppotseluev.algorate.trader.telegram.BotToken
-import com.github.ppotseluev.algorate.trader.telegram.TelegramClient
-import org.ta4j.core.analysis.criteria.pnl.{GrossProfitCriterion, ProfitLossCriterion, ProfitLossPercentageCriterion}
+import com.github.ppotseluev.algorate.trader.telegram.{BotToken, TelegramClient}
+import org.ta4j.core.analysis.criteria.pnl.ProfitLossPercentageCriterion
 
 trait EventsSink[F[_]] {
   def push(event: Event): F[Unit]
 }
 
-object EventsSink {
-  def telegram[F[_]](
+object EventsSink extends LoggingSupport {
+  private val logger = getLogger("EventsSink")
+
+  def telegram[F[_]: Sync](
       botToken: BotToken,
       chatId: String,
       client: TelegramClient[F]
-  ): EventsSink[F] = event => {
-    val (text, image) = event match {
-      case TradingSnapshot(snapshot, aggregatedStats) =>
-        val profit = new ProfitLossPercentageCriterion()
-        val longProfit = profit.calculate(snapshot.unsafe.barSeries, snapshot.unsafe.longHistory)
-        val shortProfit = profit.calculate(snapshot.unsafe.barSeries, snapshot.unsafe.shortHistory)
-        val msg = s"""
+  ): EventsSink[F] = event =>
+    ApplicativeThrow[F]
+      .catchNonFatal {
+        val (text, image) = event match {
+          case TradingSnapshot(snapshot, aggregatedStats) =>
+            val profit = new ProfitLossPercentageCriterion()
+            val longProfit =
+              profit.calculate(snapshot.unsafe.barSeries, snapshot.unsafe.longHistory)
+            val shortProfit =
+              profit.calculate(snapshot.unsafe.barSeries, snapshot.unsafe.shortHistory)
+            val msg = s"""
              |instrument: ${snapshot.ticker}
              |state: ${snapshot.state}
              |stats: ${snapshot.tradingStats}
@@ -31,21 +40,22 @@ object EventsSink {
              |aggregated stats: $aggregatedStats
              |triggeredBy: ${snapshot.triggeredBy}
              |""".stripMargin
-        val img = TradingCharts.buildImage(
-          strategyBuilder = snapshot.strategyBuilder,
-          series = snapshot.unsafe.barSeries,
-          tradingStats = Some(snapshot.tradingStats),
-          title = s"${snapshot.ticker}"
+            val img = TradingCharts.buildImage(
+              strategyBuilder = snapshot.strategyBuilder,
+              series = snapshot.unsafe.barSeries,
+              tradingStats = Some(snapshot.tradingStats),
+              title = s"${snapshot.ticker}"
+            )
+            (msg, img)
+        }
+        TelegramClient.MessageSource(
+          chatId = chatId,
+          text = text,
+          photo = image,
+          replyMarkup = None,
+          parseMode = None
         )
-        (msg, img)
-    }
-    val messageSource = TelegramClient.MessageSource(
-      chatId = chatId,
-      text = text,
-      photo = image,
-      replyMarkup = None,
-      parseMode = None
-    )
-    client.send(botToken)(messageSource)
-  }
+      }
+      .flatMap { messageSource => client.send(botToken)(messageSource) }
+      .onError { case e: Throwable => Sync[F].delay(logger.error("Can't push event", e)) }
 }
