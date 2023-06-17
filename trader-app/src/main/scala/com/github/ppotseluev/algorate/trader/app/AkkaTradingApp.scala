d62904f51d0b7f5cacc0ae8939da1167c4a269db
ppotseluev <potseluev-pasha@yandex.ru>
2023-06-17 18:46:08 +0800

    .

--- trader-app/src/main/scala/com/github/ppotseluev/algorate/trader/app/AkkaTradingApp.scala
+++ trader-app/src/main/scala/com/github/ppotseluev/algorate/trader/app/AkkaTradingApp.scala
@@ -120,7 +120,7 @@ object AkkaTradingApp extends IOApp with LazyLogging {
             val subscriber = subscription.stub[IO](
               broker,
               rate = 0.millis,
-              streamFrom = LocalDate.now.minusDays(6),
+              streamFrom = LocalDate.now.minusDays(3),
               streamTo = LocalDate.now
             )
             assets.parTraverse(subscriber.subscribe).void
